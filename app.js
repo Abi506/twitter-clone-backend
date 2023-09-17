b3c0930e64c252c5e76e6850497d1e5c0c183ebd
Abinandhan <abinandhan506@gmail.com>
2023-09-17 08:51:06 +0000

    starting the project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const format = require("date-fns/format");
+const isValid = require("date-fns/isValid");
+const toDate = require("date-fns/toDate");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const path = require("path");
+const dbPath = path.join(__dirname, "twitterClone.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server is running at http:/localhost:3000/");
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+initializeDBAndServer();
+app.post("/register/", async (request, response) => {
+  response.send("hi");
+});
+module.exports = app;
